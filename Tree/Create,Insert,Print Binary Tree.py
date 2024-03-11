3e10ff16ecd70f769323111206c1fa961b7bc152
Nitish Dobriyal <nitish.dobriyal07@gmail.com>
2024-03-11 16:14:34 +0530

    Added Tree Folder and insert/print Binary Tree

---
+++ Tree/Create,Insert,Print Binary Tree.py
@@ -0,0 +1,40 @@
+# Create a Binary Tree, Insert elements to it and Print the tree
+class Node:
+    def __init__(self,val):
+        self.left = None
+        self.right = None
+        self.val = val
+    
+    def insert(self,val):
+
+        if not self:
+            return Node(val)
+            
+        if val<self.val:
+            if(self.left is None):
+                self.left = Node(val)
+            else:
+                self.left.insert(val)
+        else:
+            
+            if(self.right is None):
+                self.right = Node(val)
+            else:
+                self.right.insert(val)
+              
+        return self  
+    def print_tree(self):
+            if self.left:
+                self.left.print_tree()
+            print(self.val, end=" ")
+            if self.right:
+                self.right.print_tree()
+                
+if __name__ == "__main__":
+    root = Node(1)
+    root.insert(2)
+    root.insert(3)
+    root.insert(4)
+    root.insert(5)
+    root.print_tree()
+            
